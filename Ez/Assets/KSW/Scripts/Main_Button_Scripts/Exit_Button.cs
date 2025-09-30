27cac8c03f82cbf9693cf68ccf957316135b380c
hanchunbae <gsiwon67@gmail.com>
2025-09-30 09:58:43 +0900

    ExitButton ADD

--- Ez/Assets/KSW/Scripts/Main_Button_Scripts/Exit_Button.cs
+++ Ez/Assets/KSW/Scripts/Main_Button_Scripts/Exit_Button.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Exit_Button : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void ExitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+        Debug.LogError("게임 종료");
     }
 }
