1d3524ed266f94d1eb6f671cf7bdbebd67f697d3
basicallybac <gongdohyeon14@gmail.com>
2025-10-13 16:35:36 +0900

    fix: Eleczone

--- Ez/Assets/GDH/1.Scripts/ElecZone.cs
+++ Ez/Assets/GDH/1.Scripts/ElecZone.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class ElecZone : MonoBehaviour
 {
-    private Culong _culong;
+    private Electron _culong;
     private float _power = 5f;
     private float _givePower;
     private void Awake()
     {
-        _culong = GameObject.Find("Electron").GetComponent<Culong>();
+        _culong = GameObject.Find("Electron").GetComponent<Electron>();
     }
 
     private void Update()
     {
-        _givePower = (_culong.CulongPower * _power)
+        _givePower = (_culong.k)
             / Mathf.Pow((_culong.gameObject.transform.position.y - gameObject.transform.position.y), 2);
         _culong.RbCompo.AddForce(Vector2.down * Mathf.Abs(_givePower));
     }
